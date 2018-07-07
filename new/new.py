022c982dc82cc59755aa07ef167ae78b5d512b87
chen1126946552 <1126946552@qq.com>
2018-07-07 21:38:01 +0800

    test

---
+++ new/new.py
@@ -0,0 +1,23 @@
+print 'hello world'
+print 'hello world2'
+print 'hello world3'
+print 'hello world4'
+print 'hello world5'
+
+<<<<<<< HEAD
+print 'hello world6'
+print 'hello world7'
+
+
+print 'hello world12'
+=======
+print 'hello world8'
+
+
+print 'hello world9'
+
+print 'hello world10'
+
+
+print 'hello world11'
+>>>>>>> test1
