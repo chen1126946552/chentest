ca7dc414a8a50f66be3f915c9ad273bbb61963ac
chen.chen <1126946552@qq.com>
2019-06-14 19:34:29 +0800

    a

--- case100/sendSlack.py
+++ case100/sendSlack.py
@@ -15,7 +15,7 @@ towho = '<!here|here>'
 # Slack频道=【chen_test】，个人调试频道
 slackQaUrl = "https://hooks.slack.com/services/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"
 # OnealertUrl对应的报警内容
-OnealertUrl = "http://api.onealert.com/alert/api/event?app=680ca4a6-bfb4-9e46-cf3b-1d2de68209b4&eventType=trigger&alarmName=中国区Ptengine&eventId=21946514-9eb2-4078-813f-a8420baf0dab-7&alarmContent=测试消息,请忽略&priority=3"
+#OnealertUrl = "http://api.onealert.com/alert/api/event?app=680ca4a6-bfb4-9e46-cf3b-1d2de68209b4&eventType=trigger&alarmName=中国区Ptengine&eventId=21946514-9eb2-4078-813f-a8420baf0dab-7&alarmContent=测试消息,请忽略&priority=3"
 
 
 
@@ -24,8 +24,8 @@ failmsg = '测试报告详情参考：'
 
 
 # -----Slack发布范围、内容详情----------
-slacktext = '{0}\n AT_DD_Online_JP_CL_Core_检查失败：\n{1}'.format(towho, failmsg)
+slacktext = '{0}\n{1} AT_DD_Online_JP_CL_Core_检查失败：\n{2}'.format(towho, failmsg, failmsg)
 
 requests.post(slackQaUrl, json={'text': slacktext})
-requests.post(OnealertUrl)
+
 
